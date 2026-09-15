b028df5ff5605df9c53331cd8e8e2dea527703c2
Gabriel <deividgabrielleandro17@Gmail.com>
2026-09-14 23:45:44 -0300

    feat: adiciona navegação por TabBar

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -6,15 +6,20 @@ namespace ATHENA
     {
         public AppShell()
         {
+            ///Paginas iniciais
+            
+            //Tela inicial
             InitializeComponent();
 
-            Routing.RegisterRoute(nameof(NewPage2), typeof(NewPage2));
-
-            Routing.RegisterRoute(nameof(NewPage1), typeof(NewPage1));
+            Routing.RegisterRoute(nameof(NewPage1), typeof(NewPage1)); //Cronometro
 
             Routing.RegisterRoute(nameof(EscolherMeta), typeof(EscolherMeta));
 
+            ///Paginas de navegação
+
             Routing.RegisterRoute(nameof(InterfaceMeta), typeof(InterfaceMeta));
+
+            Routing.RegisterRoute(nameof(NewPage2), typeof(NewPage2)); //Pagina para criar meta
 
             Routing.RegisterRoute(nameof(EditarMeta), typeof(EditarMeta));
         }
